468a73ef7c4bbfc73c7cfe8e31e1f9ffd31d317d
simeon4real <simeon.udoh45@gmail.com>
2019-09-19 20:39:08 +0100

    added webpack.js

---
+++ js/webpack.js
@@ -0,0 +1,25 @@
+// Redirecting pages
+
+function redirect_login()
+{
+document.getElementsByClassName("button")
+window.location = "/login.html";
+
+};
+
+function redirect_register()
+{
+document.getElementsByClassName("button1")
+window.location = "/signup.html";
+
+};
+
+// function redirect_message()
+//{
+//document.getElementsByClassName("login-button").submit();
+
+//window.location = "/message.html";
+
+//};
+
+
